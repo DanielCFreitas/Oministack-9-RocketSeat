153fa89fbf7b3be1f796641c1c76a2b9207e196a
Daniel <daniel.freitas@treevia.com.br>
2019-10-07 19:00:37 -0300

    Finalizado o front end, falta só enviar o formulario de cadastro de spot

---
+++ frontend/src/pages/New/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useMemo } from 'react'
+
+import camera from '../../assets/camera.svg'
+
+import './styles.css'
+
+export default function New() {
+
+    const [company, setCompany] = useState('')
+    const [techs, setTechs] = useState('')
+    const [price, setPrice] = useState('')
+    const [thumbnail, setThumbnail] = useState(null)
+
+    const preview = useMemo(() => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null;
+    }, [thumbnail])
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <label
+                id="thumbnail"
+                style={{ backgroundImage: `url(${preview})` }}
+                className={thumbnail ? 'has-thumbnail' : ''}>
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+                <img src={camera} alt="" />
+            </label>
+
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por virgula)</span></label>
+            <input
+                id="techs"
+                placeholder="Quais tecnologias usam"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="price">VALOR DA DIÁRIA * <span>(em branco para gratuito)</span></label>
+            <input
+                id="price"
+                placeholder="Valor cobrado por dia"
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className="btn">
+                Cadastrar
+            </button>
+        </form>
+    )
+}
